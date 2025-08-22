7b153a708b280dc0b986057a242da53ccadc272a
LeoAsahi <leonardo.bertonasco01@gmail.com>
2025-08-22 12:26:41 +0200

    feat(philo): scaffold initial philosopher project

---
+++ tester/test_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+		printf("0 1 took left fork\n");
+		printf("0 1 took right fork\n");
+		printf("400 1 place left fork\n");
+		printf("400 1 place right fork\n");
+		printf("402 1 is sleeping\n");
+		printf("802 1 is thinking\n");
+		return (0);
+}
