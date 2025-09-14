a40840314a26307b6b003bb0d8d2a99268e41ede
LeoAsahi <leonardo.bertonasco01@gmail.com>
2025-09-14 16:37:22 +0200

    Routine: use same highest nbr logic in unlock forks

--- srcs/logic/philosophers_routine.c
+++ srcs/logic/philosophers_routine.c
@@ -69,14 +69,24 @@ static void	lock_mutexes(t_philo *philo)
 
 static void	unlock_mutexes(t_philo *philo)
 {
-	if (philo->id < philo->shared_data->philosopher_amount)
+	pthread_mutex_t	*first_fork;
+	pthread_mutex_t	*second_fork;
+	int				left_fork_id;
+	int				right_fork_id;
+
+	left_fork_id = (philo->id - 1 + philo->shared_data->philosopher_amount - 1)\
+					% philo->shared_data->philosopher_amount;
+	right_fork_id = philo->id - 1;
+	if (left_fork_id < right_fork_id)
 	{
-		pthread_mutex_unlock(philo->fork_left_mutex);
-		pthread_mutex_unlock(philo->fork_right_mutex);
+		first_fork = philo->fork_left_mutex;
+		second_fork = philo->fork_right_mutex;
 	}
 	else
 	{
-		pthread_mutex_unlock(philo->fork_right_mutex);
-		pthread_mutex_unlock(philo->fork_left_mutex);
+		first_fork = philo->fork_right_mutex;
+		second_fork = philo->fork_left_mutex;	
 	}
+	pthread_mutex_unlock(first_fork);
+	pthread_mutex_unlock(second_fork);
 }
