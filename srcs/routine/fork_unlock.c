d8c0003c62ddf4361dbb3de0cb70990d782f1ccb
LeoAsahi <leonardo.bertonasco01@gmail.com>
2025-12-05 01:38:41 +0100

    Fix ?

--- srcs/routine/fork_unlock.c
+++ srcs/routine/fork_unlock.c
@@ -3,21 +3,15 @@
 static void	determine_unlock_order(t_philo *philo, pthread_mutex_t **first_fork,
 		pthread_mutex_t **second_fork)
 {
-	int	left_fork_id;
-	int	right_fork_id;
-
-	left_fork_id = (philo->id - 1 + philo->shared_data->philosopher_amount - 1)
-		% philo->shared_data->philosopher_amount;
-	right_fork_id = philo->id - 1;
-	if (left_fork_id < right_fork_id)
+	if (philo->id % 2 == 0)
 	{
 		*first_fork = philo->fork_left_mutex;
 		*second_fork = philo->fork_right_mutex;
 	}
 	else
 	{
-		*first_fork = philo->fork_right_mutex;
 		*second_fork = philo->fork_left_mutex;
+		*first_fork = philo->fork_right_mutex;
 	}
 }
 
